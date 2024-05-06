6af3436209ad56f6f2df4c7fa1903c2ec9a61665
DamirDenis-Tudor <denis-tudor.damir@student.tuiasi.ro>
2024-05-06 20:30:33 +0300

    work[Persistance]: Model entities added.

--- PetShop-BackEnd/PetShop-BackEnd/Program.cs
+++ PetShop-BackEnd/PetShop-BackEnd/Program.cs
@@ -1,13 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PetShop_BackEnd.Persistence.Entities;
+using PetShop_BackEnd.Persistence.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DatabaseContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +18,31 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/test", () =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
     })
     .WithName("GetWeatherForecast")
     .WithOpenApi();
 
-app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+var dbContext = new DatabaseContext();
+
+var user = new User
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    Name = "hei",
+    Email = "mama@tata.gmail.com",
+    Password = "parola",
+    UserType = "Smenar",
+    BillDetails = null
+};
+
+if (dbContext.Database.EnsureCreated())
+{
+    Console.WriteLine("Successfully created.");
 }
+
+dbContext.Add(user);
+dbContext.SaveChanges();
+
+app.Run();
