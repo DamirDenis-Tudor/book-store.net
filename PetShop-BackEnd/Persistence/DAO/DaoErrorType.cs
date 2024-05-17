6853263ec649afb3bab4769dac33812067dadca8
DamirDenis-Tudor <denis-tudor.damir@student.tuiasi.ro>
2024-05-17 13:24:37 +0300

    docs: Persistence layer documented + Class diagram.

--- PetShop-BackEnd/Persistence/DAO/DaoErrorType.cs
+++ PetShop-BackEnd/Persistence/DAO/DaoErrorType.cs
@@ -1,9 +1,41 @@
+/**************************************************************************
+ *                                                                        *
+ *  Description: DaoErrorType                                             *
+ *  Website:     https://github.com/DamirDenis-Tudor/PetShop-ProiectIP    *
+ *  Copyright:   (c) 2024, Damir Denis-Tudor                              *
+ *                                                                        *
+ *  This code and information is provided "as is" without warranty of     *
+ *  any kind, either expressed or implied, including but not limited      *
+ *  to the implied warranties of merchantability or fitness for a         *
+ *  particular purpose. You are free to use this source code in your      *
+ *  applications as long as the original copyright notice is included.    *
+ *                                                                        *
+ **************************************************************************/
+
 namespace Persistence.DAO;
 
+/// <summary>
+/// Enumeration representing various types of errors that can occur in the DAO (Data Access Object) layer.
+/// </summary>
 public enum DaoErrorType
 {
+    /// <summary>
+    /// Indicates that the requested item was not found.
+    /// </summary>
     NotFound,
+
+    /// <summary>
+    /// Indicates that the item is already registered.
+    /// </summary>
     AlreadyRegistered,
+
+    /// <summary>
+    /// Indicates that the list is empty.
+    /// </summary>
     ListIsEmpty,
+
+    /// <summary>
+    /// Indicates a general database error.
+    /// </summary>
     DatabaseError,
 }
--- PetShop-BackEnd/Persistence/DAO/Repositories/UserRepository.cs
+++ PetShop-BackEnd/Persistence/DAO/Repositories/UserRepository.cs
@@ -1,10 +1,23 @@
+/**************************************************************************
+ *                                                                        *
+ *  Description: UserRepository                                           *
+ *  Website:     https://github.com/DamirDenis-Tudor/PetShop-ProiectIP    *
+ *  Copyright:   (c) 2024, Damir Denis-Tudor                              *
+ *                                                                        *
+ *  This code and information is provided "as is" without warranty of     *
+ *  any kind, either expressed or implied, including but not limited      *
+ *  to the implied warranties of merchantability or fitness for a         *
+ *  particular purpose. You are free to use this source code in your      *
+ *  applications as long as the original copyright notice is included.    *
+ *                                                                        *
+ **************************************************************************/
+
 using Logger;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Persistence.DAL;
 using Persistence.DAO.Interfaces;
 using Persistence.DTO;
-using Persistence.DTO.Bill;
 using Persistence.DTO.User;
 
 namespace Persistence.DAO.Repositories;
