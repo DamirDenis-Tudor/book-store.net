b37808c4e6d5737086cf5a156607c9c8fed71413
PurpleFishh <asmarandei.catalin@yahoo.com>
2024-05-25 15:33:43 +0300

    work[ClientGUI]: Done all the interactive features form the GUI: Show products, filter products, the cart, enter personal details, payment, finish and send the order, view user orders and registration

--- BookStore/PresentationClient/Program.cs
+++ BookStore/PresentationClient/Program.cs
@@ -1,28 +1,39 @@
-using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Components.Web;
+
 using Persistence.DAL;
 using PresentationClient.Entities;
+using PresentationClient.Pages;
+using PresentationClient.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<ProductsScope>();
+builder.Services.AddScoped<PersonalDetailsDataScoped>();
+
+//builder.Services.AddSingleton<ProtectedLocalStorage>();
+builder.Services.AddScoped<ProtectedLocalStorage>();
+builder.Services.AddScoped<ICartService, CartServiceLocal>();
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseHsts();
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
-PersistenceFacade.Instance.SetIntegrationMode(IntegrationMode.Production);
+PersistenceAccess.Instance.SetIntegrationMode(IntegrationMode.Production);
 
 
 app.UseRouting();
-
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
