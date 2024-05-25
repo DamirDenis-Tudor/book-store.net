dda75bb57d02fd845a8588485edc7923f2150671
DamirDenis-Tudor <denis-tudor.damir@student.tuiasi.ro>
2024-05-25 16:29:35 +0300

    work[Business/Common]: Business layer structure added + Gdpr utility added.

---
+++ BookStore/Common/GdprUtility.cs
@@ -0,0 +1,56 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Common;
+
+public static class GdprUtility
+{
+    private static readonly byte[] Key = Convert.FromBase64String("y+4VFip9Kiav9xWAic9PPA==");
+    private static readonly byte[] Iv = Convert.FromBase64String("AQIDBAUGBwgJCgsMDQ4PEA==");
+    
+    
+    public static string Hash(string input)
+    {
+        var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(input));
+        var builder = new StringBuilder();
+
+        foreach (var b in bytes)
+        {
+            builder.Append(b.ToString("x2"));
+        }
+
+        return builder.ToString();
+    }
+
+    public static string Encrypt(string input)
+    {
+        using var aes = Aes.Create();
+        aes.Key = Key;
+        aes.IV = Iv;
+        
+        var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+        using var memoryStream = new MemoryStream();
+        using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+        using (var streamWriter = new StreamWriter(cryptoStream))
+        {
+            streamWriter.Write(input);
+        }
+
+        return Convert.ToBase64String(memoryStream.ToArray());
+    }
+    
+    public static string Decrypt(string input)
+    {
+        using var aes = Aes.Create();
+        aes.Key = Key;
+        aes.IV = Iv;
+        var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+        using var memoryStream = new MemoryStream(Convert.FromBase64String(input));
+        using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+        using var streamReader = new StreamReader(cryptoStream);
+        
+        return streamReader.ReadToEnd();
+    }
+}
