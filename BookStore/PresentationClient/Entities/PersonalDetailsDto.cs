750fc30d4dcf9f9056af552cc4f226df5da5dfe5
PurpleFishh <asmarandei.catalin@yahoo.com>
2024-05-27 00:18:34 +0300

    work[ClientGUI]: Integreted the business conection with the GUI

---
+++ BookStore/PresentationClient/Entities/PersonalDetailsDto.cs
@@ -0,0 +1,29 @@
+﻿using Business.BTO;
+using Persistence.DTO.Bill;
+using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.Entities
+{
+	public class PersonalDetailsDto
+	{
+		[Required]
+		public string Address { get; set; }
+		[Required]
+		public string Telephone { get; set; }
+		[Required]
+		public string Country { get; set; }
+		[Required]
+		public string City { get; set; }
+		[Required]
+		public string PostalCode { get; set; }
+
+		public void Deserialize(BillDto bill)
+		{
+			Address = bill.Address;
+			Telephone = bill.Telephone;
+			Country = bill.Country;
+			City = bill.City;
+			PostalCode = bill.PostalCode;
+		}
+	}
+}
